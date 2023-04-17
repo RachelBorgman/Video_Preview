74b57c1300363eafcadcc0e1a8f90f2704766807
RachelBorgman <123611004+RachelBorgman@users.noreply.github.com>
2023-04-17 14:05:42 -0400

    Finished Assignment of Video Preview
    Finished Assignment of Video Preview including hovering to play and pause

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("page loaded...");
+
+// var x = document.createElement("VIDEO");
+
+document.getElementById("myVideo").muted = true;
+
+function over(element) {
+    myVideo.play();    
+}
+    
+function out(element) {
+    myVideo.pause();    
+}
+
